05d7e52b82646231c0c5c156c97e0e5b53d3fab3
Najeebullah Joya <najeebullah.joya79@gmail.com>
2022-04-07 15:23:36 +0430

    fixed numbers format

--- script.js
+++ script.js
@@ -17,11 +17,15 @@ function updateclock(){
             hou = hou - 12;
             per = "PM"
         }
+        Number.prototype.pad = function(digits){
+            for(var n = this.toString(); n.length < digits; n = 0+n);
+            return n;
+        }
 
         let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
         let wdays = ["Sunday","Monday","Tuesday","Wednesday","Thursday", "Friday","Saturday"]; 
         let elements = ["wday","month","mday","year","hour","minute","second","period"];
-        let values = [wdays[wday], months[month], mday, year, hou, minute, second, per];
+        let values = [wdays[wday], months[month], mday.pad(2), year, hou.pad(2), minute.pad(2), second.pad(2), per];
 
        
 
